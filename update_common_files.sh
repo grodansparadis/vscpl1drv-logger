d4a55c8eb95d2251c734f0e411d561e571afa1a2
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-09-26 21:35:24 +0200

    Adding dependent files

--- update_common_files.sh
+++ update_common_files.sh
@@ -5,10 +5,17 @@ cd _vscp_common_general
 rm -f *
 wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/dllist.c
 wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/dllist.h
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/crc.h
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/crc.c
 cd ..
 # VSCP Common files
 cd _vscp_common
 rm -f *
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/canal.h
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/canal_macro.h
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/vscp.h
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/vscp_class.h
+wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/vscp_type.h
 wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/guid.cpp
 wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/guid.h
 cd ..
