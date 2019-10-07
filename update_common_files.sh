5c8e5c5a406a8e56c4a8c8c60cceea9c0b9803e0
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-10-07 17:04:12 +0200

    Fixed typos

--- update_common_files.sh
+++ update_common_files.sh
@@ -6,20 +6,20 @@
 # General common files
 cd _vscp_common_general
 rm -f *
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/dllist.c
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/dllist.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/crc.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/common/crc.c
+cp  ${VSCP_PATH}/src/common/dllist.c .
+cp  ${VSCP_PATH}/src/common/dllist.h .
+cp  ${VSCP_PATH}/src/common/crc.h .
+cp  ${VSCP_PATH}/src/common/crc.c .
 cd ..
 # VSCP Common files
 cd _vscp_common
 rm -f *
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/version.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/canal.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/canal_macro.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/vscp.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/vscp_class.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/vscp_type.h
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/guid.cpp
-wget https://raw.githubusercontent.com/grodansparadis/vscp/master/src/vscp/common/guid.h
+cp  ${VSCP_PATH}/src/vscp/common/version.h .
+cp  ${VSCP_PATH}/src/vscp/common/canal.h .
+cp  ${VSCP_PATH}/src/vscp/common/canal_macro.h .
+cp  ${VSCP_PATH}/src/vscp/common/vscp.h .
+cp  ${VSCP_PATH}/src/vscp/common/vscp_class.h .
+cp  ${VSCP_PATH}/src/vscp/common/vscp_type.h .
+cp  ${VSCP_PATH}/src/vscp/common/guid.cpp .
+cp  ${VSCP_PATH}/src/vscp/common/guid.h .
 cd ..
